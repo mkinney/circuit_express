afcaba08cb19e0555c35aa1588a68e4dbc80187a
Mike Kinney <mike.kinney@gmail.com>
2019-05-20 19:56:00 -0700

    light up all lights; add ability to change sensitivity; option to have lights on when switch to right

--- loud_leds.js
+++ loud_leds.js
@@ -1,24 +1,32 @@
-let lastLevel = 0
 let level = 0
 let pixels = light.createStrip(pins.A1, 30)
 let color = Colors.White
-if (SwitchDirection.Left) {
-    color = Colors.Blue
-} else {
-    color = Colors.White
-}
+let sens = 120
+pixels.setBrightness(255)
+light.setBrightness(255)
 forever(function () {
+
+    if (input.buttonA.isPressed()) {
+        sens = sens - 5
+    }
+    if (input.buttonB.isPressed()) {
+        sens = sens + 5
+    }
+
+    if (input.switchRight()) {
+        sens = 0
+    }
+
     level = input.soundLevel()
-    if (lastLevel != level) {
-        light.clear()
-        pixels.clear()
-        for (let i = 0; i <= pixels.length() / 255 * level - 14; i++) {
-            pixels.setPixelColor(i, color)
-        }
-        for (let j = 0; j <= 10 / 255 * level - 5; j++) {
-            light.setPixelColor(j, color)
-        }
-        lastLevel = level
+    color = Colors.Black
+    if (level > sens) {
+        color = Colors.Blue
+    }
+    for (let k = 0; k <= pixels.length(); k++) {
+        pixels.setPixelColor(k, color)
+    }
+    for (let l = 0; l <= 10; l++) {
+        light.setPixelColor(l, color)
     }
 })
 
